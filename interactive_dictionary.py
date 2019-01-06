1a8c06098ad4b11e89376b84f96320318bd4aabf
vasu shah <vasushah76@gmail.com>
2019-01-06 16:07:03 -0500

    Implementation for recommending the best match

--- interactive_dictionary.py
+++ interactive_dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 #load JSON data
 data = json.load(open("data.json"))
@@ -13,6 +14,18 @@ def getMeaning(w):
     #if-else to check word exist in our data or not
     if w in data:
         return data[w]
+    #give matching word
+    elif len(get_close_matches(w,data.keys())) > 0:
+        close_match = get_close_matches(w,data.keys())[0]
+        print("Did you mean %s instead? Enter Y if yes or N if no: " % close_match)
+        choice = input()
+        choice = choice.lower()
+        if choice == 'y':
+            return data[close_match]
+        elif choice == 'n':
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "Sorry, We didn't understand your entry."
     else:
         return "The word doesn't exist. Please double check it."
 
@@ -20,4 +33,8 @@ def getMeaning(w):
 meaning = getMeaning(word)
 
 #printing meaning of the word in console
-print(meaning)
+if type(meaning) == list:
+    for item in meaning:
+        print(item)
+else:
+    print(meaning)
